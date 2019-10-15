5281960e41c68d601fbacf6c4dca422eb67b6df9
Captaincrogs <2003829@talnet.nl>
2019-10-15 12:14:27 +0200

    Uitwerking van de exercise

---
+++ Variable.php
@@ -0,0 +1,15 @@
+<?php
+$tekst1 = "hello world";
+$tekst2 = "mijn bestand";
+$tekst3 = "mijn bestand nummer2";
+$tekst4 = "mijn bestand nummer3";
+
+    echo $tekst1;
+    echo PHP_EOL;
+    echo $tekst2;
+    echo PHP_EOL;
+    echo $tekst3;
+    echo PHP_EOL;
+    echo $tekst4;
+    echo PHP_EOL;
+?>
